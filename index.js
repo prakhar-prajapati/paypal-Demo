f8fa56a4ab8c522dd0cd52394cf994abc564e799
prakhar prajapati <46026860+prakhar-prajapati@users.noreply.github.com>
2020-05-20 11:55:25 +0530

    Strip payout demo

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const ejs = require('ejs');
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AWiCry6CMXUKUEPkfTi_esu90eAQor01CDelhVfYS9bSvuJTs__BgTDqmV2utDImXpCEGoktaTiIVg87',
+  'client_secret': 'EDIloNPKGfxh958ypUUvyrs3QL3pD49NgTha5NsIlH42FmaTDazyDJ-HaKqQ9E0Hb-BTgdq2zsK6k3GU'
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.post('/pay', (req, res) => {
+    const create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/success",
+          "cancel_url": "http://localhost:3000/cancel"
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "Hat",
+                  "sku": "001",
+                  "price": "25.00",
+                  "currency": "USD",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          },
+          "description": "papal testing"
+      }]
+  };
+  
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0;i < payment.links.length;i++){
+          if(payment.links[i].rel === 'approval_url'){
+            res.redirect(payment.links[i].href);
+          }
+        }
+    }
+  });
+  
+  });
+
+  app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+  
+    const execute_payment_json = {
+      "payer_id": payerId,
+      "transactions": [{
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          }
+      }]
+    };
+  
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log(JSON.stringify(payment));
+          res.send('Success');
+      }
+  });
+  });
+
+  app.get('/cancel', (req, res) => res.send('Cancelled'));
+app.listen(3000 ,()=>console.log('Paypal demo run on port 3000'))
